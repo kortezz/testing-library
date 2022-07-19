5f596313c619b30b9acbd06e1db3b7e3d2817fed
murat <kortezz>
2022-07-20 02:11:01 +0300

    Added form and tests

--- src/form/Form.tsx
+++ src/form/Form.tsx
@@ -8,10 +8,14 @@ interface FormProps {
 const Form = (props: FormProps) => {
 
     const [name, setName] = useState('');
+    const [selected, setSelected] = useState('one');
+    const [checkbox, setCheckbox] = useState(false);
 
     return (
-        <div>
-            <form onSubmit={event => props.onSubmit(event)}>
+        <>
+            {console.log('div')}
+            <form data-testid="form" onSubmit={event => props.onSubmit(event)}
+                  style={{display: "flex", flexDirection: "column", justifyContent: "flex-end", gap: '10px'}}>
                 <input
                     placeholder="name"
                     onChange={event => setName(event.target.value)}
@@ -20,9 +24,33 @@ const Form = (props: FormProps) => {
                     name="Name"
                     required
                 />
-                <button type="submit">Submit</button>
+              <input
+                    placeholder="password"
+                    type='password'
+                    name="Password"
+                    required
+                />
+              <select
+                value={selected}
+                onChange={(e) => setSelected(e.target.value)}
+              >
+                <option value={"default"} disabled>
+                  Choose an option
+                </option>
+                <option value={"one"}>One</option>
+                <option value={"two"}>Two</option>
+                <option value={"three"}>Three</option>
+              </select>
+              <input type="checkbox" defaultChecked={checkbox}
+                     onChange={(e) => {
+                       setCheckbox((prevState) => !prevState)
+                       console.log(checkbox)
+                     }
+              } />
+              <span data-testid="selected-value">{selected}</span>
+              <button type="submit">Submit</button>
             </form>
-        </div>
+        </>
     );
 };
 
--- src/form/__test__/Form.test.tsx
+++ src/form/__test__/Form.test.tsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import '../Form';
+import Form from "../Form";
 
 describe('Form', () => {
-   it('exists header', () => {
+   it('exists form', () => {
+     render(<Form onSubmit={jest.fn()} />)
 
-   })
+     expect(screen.getByTestId("form")).toBeInTheDocument();
+   });
+
+  it('exists name', () => {
+     render(<Form onSubmit={jest.fn()} />)
+
+     expect(screen.getByPlaceholderText("name")).toBeInTheDocument();
+   });
+
+  it('exists password', () => {
+     render(<Form onSubmit={jest.fn()} />)
+
+     expect(screen.getByPlaceholderText("password")).toBeInTheDocument();
+   });
+
+  it('required buttons', async () => {
+    let onSubmit = jest.fn();
+    render(<Form onSubmit={onSubmit}/>)
+
+    expect(screen.getByPlaceholderText('name')).toHaveAttribute("required")
+    expect(screen.getByPlaceholderText('password')).toHaveAttribute("required")
+    expect(screen.getByRole('checkbox')).not.toBeChecked()
+  });
+
+  it('check onSubmit', () => {
+    let onSubmit = jest.fn();
+    render(<Form onSubmit={onSubmit}/>)
+
+    let button = screen.getByRole('button');
+    fireEvent.click(button)
+
+    expect(button.textContent).toBe('Submit');
+    expect(onSubmit).toBeCalledTimes(1);
+  })
+
 });
